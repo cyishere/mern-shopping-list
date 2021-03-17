9c9d296badaf4ec152c7986693174a3637dd8ed9
irini <cyishere@gmail.com>
2021-03-17 13:48:12 +0800

    setup client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello Voyager</h1>
+    </div>
+  );
+}
+
+export default App;
